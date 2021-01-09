b664e953489043a08665d0d76df843ef9f276e06
mhyeun <mhyeun131@gmail.com>
2021-01-09 01:38:29 -0500

    Revert changes

--- src/emojify_tweets.py
+++ src/emojify_tweets.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from transform_tweet import transform_tweets
 import json
-import git
 
 app = Flask(__name__)
 
@@ -24,15 +23,3 @@ def send_emojified_tweets():
         return jsonify(transformed_tweets)
     except:
         return "Twitter username or number of tweets invalid.", 400
-
-
-@app.route("/update_server", methods=["POST"])
-def webhook():
-    if request.method == "POST":
-        repo = git.Repo("/mhyeun/twitter-emojify-api")
-        origin = repo.remotes.origin
-
-        origin.pull()
-        return "Updated PythonAnywhere successfully", 200
-    else:
-        return "Wrong event type", 400
